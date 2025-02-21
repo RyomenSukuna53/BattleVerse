afdd2bf8d3d003c74538ff768e0e86b89bf0a289
RyomenSukuna53 <sufyan532011@gmail.com>
2025-02-21 14:47:11 +0530

    Update __main__.py

--- BattleVerse /__main__.py
+++ BattleVerse /__main__.py
@@ -8,4 +8,4 @@ if __name__ == "__main__":
     bot.run()
     with bot:
        bot.send_message(chat_id=-1002393279895,
-          text=f"**BattleVerse has started [!](https://files.catbox.moe/s911b7.jpg)**")
+          text=f"**BattleVerse has started [!]**")
